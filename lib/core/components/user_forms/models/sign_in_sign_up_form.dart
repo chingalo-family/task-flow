178f86de763b687287854834acc214ff8d873be6
Joseph Chingalo <profschingalo@gmail.com>
2022-01-11 18:38:10 +0300

    feat: 🎸 sign in and sign out forms
    Initiate form contents for sign in and sign up forms

--- lib/core/components/user_forms/models/sign_in_sign_up_form.dart
+++ lib/core/components/user_forms/models/sign_in_sign_up_form.dart
@@ -3,7 +3,32 @@ import 'package:task_manager/models/form_section.dart';
 import 'package:task_manager/models/input_field.dart';
 
 class SignInSignUpForm {
-  static List<FormSection> getFormSections(Color textColor) {
+  static List<FormSection> getSignInFormSections(Color textColor) {
+    return [
+      FormSection(
+        name: '',
+        color: textColor,
+        inputFields: [
+          InputField(
+            id: 'title',
+            name: 'Title',
+            valueType: 'TEXT',
+            inputColor: textColor,
+            labelColor: textColor,
+          ),
+          InputField(
+            id: 'description',
+            name: 'Desctiption',
+            valueType: 'TEXT',
+            inputColor: textColor,
+            labelColor: textColor,
+          ),
+        ],
+      ),
+    ];
+  }
+
+  static List<FormSection> getSignUpFormSections(Color textColor) {
     return [
       FormSection(
         name: '',
