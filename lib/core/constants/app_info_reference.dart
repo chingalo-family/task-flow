0df33ed401b89f6a3497f9e84af5f4cd0d963af1
Joseph Chingalo <profschingalo@gmail.com>
2022-01-11 18:03:11 +0300

    feat: 🎸 Support for default app version of the app

--- lib/core/constants/app_info_reference.dart
+++ lib/core/constants/app_info_reference.dart
@@ -1,4 +1,5 @@
 class AppInfoReference {
   static final String androidId = 'chingalo.family.todo_app';
   static final String currentAppName = 'chingalo todo app';
+  static final String currentAppVersion = '1.0.0';
 }
