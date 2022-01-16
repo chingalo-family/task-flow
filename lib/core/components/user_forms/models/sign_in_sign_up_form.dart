a4f4dd471d34681ebd634b9ab3ecd02b7f32ddb8
Joseph Chingalo <profschingalo@gmail.com>
2022-01-17 01:38:29 +0300

    feat: 🎸 Support for model for rendering sign up forms

--- lib/core/components/user_forms/models/sign_in_sign_up_form.dart
+++ lib/core/components/user_forms/models/sign_in_sign_up_form.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:task_manager/models/form_section.dart';
 import 'package:task_manager/models/input_field.dart';
+import 'package:task_manager/models/input_field_option.dart';
 
 class SignInSignUpForm {
   static List<FormSection> getSignInFormSections(Color textColor) {
@@ -36,18 +37,62 @@ class SignInSignUpForm {
         color: textColor,
         inputFields: [
           InputField(
-            id: 'title',
-            name: 'Title',
+            id: 'fullName',
+            name: 'Full Name',
+            valueType: 'TEXT',
+            inputColor: textColor,
+            labelColor: textColor,
+          ),
+          //@TODO adding support for email type input field
+          InputField(
+            id: 'email',
+            name: 'E-mail',
             valueType: 'TEXT',
             inputColor: textColor,
             labelColor: textColor,
           ),
           InputField(
-            id: 'description',
-            name: 'Desctiption',
+            id: 'phoneNumber',
+            name: 'Phone Number',
+            valueType: 'PHONE_NUMBER',
+            inputColor: textColor,
+            labelColor: textColor,
+          ),
+          InputField(
+            id: 'gender',
+            name: 'Gender',
             valueType: 'TEXT',
             inputColor: textColor,
             labelColor: textColor,
+            options: [
+              InputFieldOption(
+                code: 'gender_male',
+                name: 'Male',
+              ),
+              InputFieldOption(
+                code: 'gender_female',
+                name: 'Female',
+              ),
+              InputFieldOption(
+                code: 'gender_other',
+                name: 'Other',
+              ),
+            ],
+          ),
+          InputField(
+            id: 'username',
+            name: 'Username',
+            valueType: 'TEXT',
+            inputColor: textColor,
+            labelColor: textColor,
+          ),
+          InputField(
+            id: 'password',
+            name: 'Password',
+            valueType: 'TEXT',
+            inputColor: textColor,
+            labelColor: textColor,
+            isPassowrdField: true,
           ),
         ],
       ),
