22c892ec145988ea030a97c56188e94d2a5a4b14
Joseph Chingalo <profschingalo@gmail.com>
2022-01-25 12:38:31 +0300

    feat: 🎸 Add support get user and user groups state into state

--- lib/modules/splash/splash.dart
+++ lib/modules/splash/splash.dart
@@ -5,7 +5,13 @@ import 'package:provider/provider.dart';
 import 'package:task_manager/app_state/app_info_state/app_info_state.dart';
 import 'package:task_manager/app_state/app_theme_state/app_theme_state.dart';
 import 'package:task_manager/app_state/task_state/task_state.dart';
+import 'package:task_manager/app_state/user_state/user_group_state.dart';
+import 'package:task_manager/app_state/user_state/user_state.dart';
 import 'package:task_manager/core/services/theme_service.dart';
+import 'package:task_manager/core/services/user_group_service.dart';
+import 'package:task_manager/core/services/user_service.dart';
+import 'package:task_manager/models/user.dart';
+import 'package:task_manager/models/user_group.dart';
 import 'package:task_manager/modules/task_manager/task_manager_home.dart';
 
 class Splash extends StatefulWidget {
@@ -23,26 +29,28 @@ class _SplashState extends State<Splash> {
   }
 
   setAppThemeAndInitialData(BuildContext context) async {
-    AppThemeState appThemeState =
-        Provider.of<AppThemeState>(context, listen: false);
-    Timer(Duration(seconds: 1), () {
-      ThemeServices.getCurrentTheme().then((theme) {
-        appThemeState.setCurrentTheme(theme);
-        Provider.of<TaskState>(context, listen: false).initiateTaskList();
-        Provider.of<AppInfoState>(context, listen: false).setCurrentAppInfo();
-        Timer(
-          Duration(
-            seconds: 2,
-          ),
-          () => Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(
-              builder: (_) => TaskMangerHome(),
-            ),
-          ),
-        );
-      });
-    });
+    User? user = await UserService().getCurrentUser();
+    AppThemeState appThemeState = Provider.of<AppThemeState>(context, listen: false);
+    String theme = await ThemeServices.getCurrentTheme();
+    appThemeState.setCurrentTheme(theme);
+    Provider.of<TaskState>(context, listen: false).initiateTaskList();
+    Provider.of<AppInfoState>(context, listen: false).setCurrentAppInfo();
+    if (user != null && user.isLogin) {
+      List<UserGroup> userGroups = await UserGroupService().getUserGroupsByUserId(userId: user.id);
+      Provider.of<UserState>(context, listen: false).setCurrentUser(user);
+      Provider.of<UserGroupState>(context, listen: false).setCurrentUserGroups(userGroups);
+    }
+    Timer(
+      Duration(
+        seconds: 2,
+      ),
+      () => Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (_) => TaskMangerHome(),
+        ),
+      ),
+    );
   }
 
   @override
