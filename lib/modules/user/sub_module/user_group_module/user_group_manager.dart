f26d00350524ef4a0949ea20aca0a1ee737549a1
Joseph Chingalo <profschingalo@gmail.com>
2022-01-31 12:18:12 +0300

    feat: 🎸 Add page for manage user groups

--- lib/modules/user/sub_module/user_group_module/user_group_manager.dart
+++ lib/modules/user/sub_module/user_group_module/user_group_manager.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:task_manager/app_state/user_state/user_state.dart';
+import 'package:task_manager/app_state/app_theme_state/app_theme_state.dart';
+import 'package:task_manager/app_state/user_state/user_group_state.dart';
 import 'package:task_manager/core/components/app_bar_container.dart';
+import 'package:task_manager/core/components/material_card.dart';
 import 'package:task_manager/core/constants/app_contant.dart';
-import 'package:task_manager/core/utils/app_util.dart';
-import 'package:task_manager/models/user.dart';
-import 'package:task_manager/modules/user/user_action_sheet.dart';
+import 'package:task_manager/core/services/theme_service.dart';
+import 'package:task_manager/models/user_group.dart';
+import 'package:task_manager/modules/user/sub_module/user_group_module/conponent/user_group_list_container.dart';
 
 class UserGroupManager extends StatelessWidget {
   const UserGroupManager({Key? key}) : super(key: key);
@@ -32,7 +34,48 @@ class UserGroupManager extends StatelessWidget {
               horizontal: 10.0,
               vertical: 10.0,
             ),
-            child: Container(),
+            child: Consumer<AppThemeState>(builder: (context, appThemeState, child) {
+              String currentTheme = appThemeState.currentTheme;
+              return Consumer<UserGroupState>(builder: (context, userGroupState, child) {
+                List<UserGroup> userGroups = userGroupState.currentUserGroups;
+                return Column(
+                  children: [
+                    Container(
+                      child: MaterialCard(
+                        body: Container(
+                          width: double.maxFinite,
+                          margin: EdgeInsets.symmetric(vertical: 10.0),
+                          padding: EdgeInsets.symmetric(
+                            vertical: 5.0,
+                            horizontal: 10.0,
+                          ),
+                          child: Text(
+                            'Search field',
+                            style: const TextStyle().copyWith(
+                              color: currentTheme == ThemeServices.darkTheme
+                                  ? AppContant.darkTextColor
+                                  : AppContant.ligthTextColor,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    ...userGroups
+                        .where((UserGroup userGroup) {
+                          //@TODO  add support for hahndling search
+                          return true;
+                        })
+                        .map(
+                          (UserGroup userGroup) => UserGroupListContainer(
+                            currentTheme: currentTheme,
+                            userGroup: userGroup,
+                          ),
+                        )
+                        .toList()
+                  ],
+                );
+              });
+            }),
           ),
         ),
       ),
