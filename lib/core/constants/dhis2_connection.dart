dfcbb42fdaf29696165f4990c278f1df808c27b3
Joseph Chingalo <profschingalo@gmail.com>
2024-12-27 18:09:42 +0300

    feat: upgrade app packages

---
+++ lib/core/constants/dhis2_connection.dart
@@ -0,0 +1,6 @@
+class Dhis2Connection {
+  static const String baseUrl = 'vmi1245558.contaboserver.net';
+  static const String subBaseUrl = 'dukamkononi';
+  static const username = 'admin';
+  static const password = 'Chingalo@111987';
+}
