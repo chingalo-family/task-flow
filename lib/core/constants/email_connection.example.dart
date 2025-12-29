ca59dc0edc9ce72496f38548a8d6ac08380f91e8
Joseph Chingalo <profschingalo@gmail.com>
2025-12-29 14:06:45 +0300

    feat: add email services

---
+++ lib/core/constants/email_connection.example.dart
@@ -0,0 +1,6 @@
+class EmailConnection {
+  static const senderEmail = 'name_of_sender';
+  static const senderName = 'gmail_email_account';
+  static const password = 'password';
+  static const adminEmails = [];
+}
