442366a05ad6f20faabddc705186fef59db04f45
Joseph Chingalo <profschingalo@gmail.com>
2026-01-02 11:19:56 +0300

    chore: add templates

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:task_manager/my_app.dart';
 
-void main() {
+import 'package:task_manager/core/services/db_service.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await DBService().init();
   runApp(const MyApp());
 }
